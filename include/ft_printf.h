1f7d06403fce4528e9f2016d697f1ce93f905640
Alibi <ayermeko@student.42prague.com>
2023-11-09 14:08:32 +0100

    modified files

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 12:50:21 by ayermeko          #+#    #+#             */
-/*   Updated: 2023/11/05 12:53:37 by ayermeko         ###   ########.fr       */
+/*   Updated: 2023/11/06 15:20:12 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+
+int ft_printf(const char *, ...);
 
 #endif
